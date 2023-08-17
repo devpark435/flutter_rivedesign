5da6e6ba76a3187e529a2d03b39adabea6288279
devpark435 <devpark435@gmail.com>
2023-08-17 18:46:03 +0900

    add #1 : move dialog close button

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -112,37 +112,12 @@ Future<Object?> customSigninDialog(context) {
               child: Scaffold(
                 backgroundColor: Colors.transparent,
                 body: Center(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
+                  child: Stack(
+                    clipBehavior: Clip.none,
                     children: [
-                      Stack(
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          Positioned(
-                            right: 10,
-                            child: Container(
-                              width: 45,
-                              height: 45,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                border: Border.all(
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .primaryContainer,
-                                    width: 2),
-                                // color: Theme.of(context)
-                                //     .colorScheme
-                                //     .primaryContainer,
-                              ),
-                              child: IconButton(
-                                  onPressed: () {},
-                                  icon: Icon(
-                                    Icons.close,
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .primaryContainer,
-                                  )),
-                            ),
-                          ),
                           Center(
                             child: Padding(
                               padding:
@@ -157,156 +132,180 @@ Future<Object?> customSigninDialog(context) {
                                         fontWeightDelta: 3),
                               ),
                             ),
-                          )
-                        ],
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10),
-                        child: Text("Take your first picture \n&\n share it",
-                            textAlign: TextAlign.center,
-                            style: Theme.of(context)
-                                .textTheme
-                                .labelLarge!
-                                .apply(color: Colors.black)),
-                      ),
-                      Form(
-                          child: Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 10.0),
-                            child: TextFormField(
-                              decoration: InputDecoration(
-                                prefixIcon: const Icon(Icons.email),
-                                labelText: "Email",
-                                labelStyle: Theme.of(context)
-                                    .textTheme
-                                    .labelMedium!
-                                    .apply(color: Colors.black),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(25),
-                                ),
-                              ),
-                            ),
                           ),
                           Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 10.0),
-                            child: TextFormField(
-                              decoration: InputDecoration(
-                                prefixIcon: const Icon(Icons.lock),
-                                labelText: "Password",
-                                labelStyle: Theme.of(context)
+                            padding: const EdgeInsets.symmetric(vertical: 10),
+                            child: Text(
+                                "Take your first picture \n&\n share it",
+                                textAlign: TextAlign.center,
+                                style: Theme.of(context)
                                     .textTheme
-                                    .labelMedium!
-                                    .apply(color: Colors.black),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(25),
+                                    .labelLarge!
+                                    .apply(color: Colors.black)),
+                          ),
+                          Form(
+                              child: Column(
+                            children: [
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 10.0),
+                                child: TextFormField(
+                                  decoration: InputDecoration(
+                                    prefixIcon: const Icon(Icons.email),
+                                    labelText: "Email",
+                                    labelStyle: Theme.of(context)
+                                        .textTheme
+                                        .labelMedium!
+                                        .apply(color: Colors.black),
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(25),
+                                    ),
+                                  ),
                                 ),
                               ),
-                            ),
-                          )
-                        ],
-                      )),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10.0),
-                        child: ElevatedButton.icon(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor:
-                                Theme.of(context).colorScheme.primaryContainer,
-                            minimumSize: const Size(double.infinity, 50),
-                            shape: const RoundedRectangleBorder(
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(10),
-                                bottomLeft: Radius.circular(25),
-                                topRight: Radius.circular(25),
-                                bottomRight: Radius.circular(10),
-                              ),
-                            ),
-                          ),
-                          icon: Icon(
-                            Icons.arrow_right_alt,
-                            color: Theme.of(context)
-                                .colorScheme
-                                .onPrimaryContainer,
-                          ),
-                          label: Text(
-                            "Sign In",
-                            style: Theme.of(context).textTheme.bodyLarge!.apply(
-                                color: Theme.of(context)
-                                    .colorScheme
-                                    .onPrimaryContainer),
-                          ),
-                        ),
-                      ),
-                      Row(children: [
-                        Expanded(
-                          child: Container(
-                            margin: const EdgeInsets.symmetric(horizontal: 20),
-                            child: const Divider(
-                              color: Colors.black26,
-                              height: 36,
-                            ),
-                          ),
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 16),
-                          child: Text(
-                            "OR",
-                            style: TextStyle(color: Colors.black26),
-                          ),
-                        ),
-                        Expanded(
-                          child: Container(
-                            margin: const EdgeInsets.symmetric(horizontal: 20),
-                            child: const Divider(
-                              color: Colors.black26,
-                              height: 36,
-                            ),
-                          ),
-                        ),
-                      ]),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: Text("Sign up with E-mail, Apple",
-                            style: Theme.of(context)
-                                .textTheme
-                                .labelLarge!
-                                .apply(color: Colors.black54)),
-                      ),
-                      Column(
-                        children: [
-                          ElevatedButton.icon(
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 10.0),
+                                child: TextFormField(
+                                  decoration: InputDecoration(
+                                    prefixIcon: const Icon(Icons.lock),
+                                    labelText: "Password",
+                                    labelStyle: Theme.of(context)
+                                        .textTheme
+                                        .labelMedium!
+                                        .apply(color: Colors.black),
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(25),
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          )),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 10.0),
+                            child: ElevatedButton.icon(
+                              onPressed: () {},
                               style: ElevatedButton.styleFrom(
-                                minimumSize: const Size(double.infinity, 40),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
                                 backgroundColor: Theme.of(context)
                                     .colorScheme
                                     .primaryContainer,
+                                minimumSize: const Size(double.infinity, 50),
+                                shape: const RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(10),
+                                    bottomLeft: Radius.circular(25),
+                                    topRight: Radius.circular(25),
+                                    bottomRight: Radius.circular(10),
+                                  ),
+                                ),
                               ),
-                              onPressed: () {},
                               icon: Icon(
-                                Icons.email,
+                                Icons.arrow_right_alt,
                                 color: Theme.of(context)
                                     .colorScheme
                                     .onPrimaryContainer,
                               ),
                               label: Text(
-                                "Sign in with E-mail",
-                                style: TextStyle(
-                                  fontSize: 17,
-                                  color: Theme.of(context)
-                                      .colorScheme
-                                      .onPrimaryContainer,
+                                "Sign In",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyLarge!
+                                    .apply(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .onPrimaryContainer),
+                              ),
+                            ),
+                          ),
+                          Row(children: [
+                            Expanded(
+                              child: Container(
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 20),
+                                child: const Divider(
+                                  color: Colors.black26,
+                                  height: 36,
                                 ),
-                              )),
+                              ),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 16),
+                              child: Text(
+                                "OR",
+                                style: TextStyle(color: Colors.black26),
+                              ),
+                            ),
+                            Expanded(
+                              child: Container(
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 20),
+                                child: const Divider(
+                                  color: Colors.black26,
+                                  height: 36,
+                                ),
+                              ),
+                            ),
+                          ]),
                           Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 10.0),
-                            child: SignInWithAppleButton(onPressed: () {}),
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: Text("Sign up with E-mail, Apple",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .labelLarge!
+                                    .apply(color: Colors.black54)),
+                          ),
+                          Column(
+                            children: [
+                              ElevatedButton.icon(
+                                  style: ElevatedButton.styleFrom(
+                                    minimumSize:
+                                        const Size(double.infinity, 40),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                    backgroundColor: Theme.of(context)
+                                        .colorScheme
+                                        .primaryContainer,
+                                  ),
+                                  onPressed: () {},
+                                  icon: Icon(
+                                    Icons.email,
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .onPrimaryContainer,
+                                  ),
+                                  label: Text(
+                                    "Sign in with E-mail",
+                                    style: TextStyle(
+                                      fontSize: 17,
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onPrimaryContainer,
+                                    ),
+                                  )),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 10.0),
+                                child: SignInWithAppleButton(onPressed: () {}),
+                              ),
+                            ],
                           ),
                         ],
                       ),
+                      Positioned(
+                          left: 0,
+                          right: 0,
+                          bottom: -35,
+                          child: CircleAvatar(
+                            radius: 16,
+                            backgroundColor:
+                                Theme.of(context).colorScheme.background,
+                            child: Icon(
+                              Icons.close,
+                              color: Theme.of(context).colorScheme.onBackground,
+                            ),
+                          ))
                     ],
                   ),
                 ),
