648bcf9dc37aea7f54282d827212039eeb1c75d2
devpark435 <devpark435@gmail.com>
2023-08-19 19:41:44 +0900

    edit #1 : rename home to bottom_nav

--- lib/widget/bottom_nav.dart
+++ lib/widget/bottom_nav.dart
@@ -1,26 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class BottomNavigation extends StatefulWidget {
+  const BottomNavigation({super.key});
 
+  @override
+  State<BottomNavigation> createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+  int _selectIndex = 0;
+  static final List<Widget> _widgetOptions = <Widget>[
+    Text("Home"),
+    Text("Location"),
+    Text("Category"),
+    Text("User"),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-          child: Center(
-        child: Text("Home Screen"),
-      )),
+      body: SafeArea(child: _widgetOptions.elementAt(_selectIndex)),
       bottomNavigationBar: GNav(
           haptic: true,
+          curve: Curves.easeIn,
+          gap: 8,
           tabBackgroundColor: Theme.of(context).colorScheme.primaryContainer,
           backgroundColor: Theme.of(context).colorScheme.background,
           tabBorderRadius: 25,
           activeColor: Theme.of(context).colorScheme.onPrimaryContainer,
-          curve: Curves.easeIn,
           tabMargin: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
           padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
-          gap: 8,
           tabs: const [
             GButton(
               icon: Icons.home,
@@ -29,7 +38,13 @@ class HomeScreen extends StatelessWidget {
             GButton(icon: Icons.location_on, text: 'location'),
             GButton(icon: Icons.list, text: 'category'),
             GButton(icon: Icons.person_pin_rounded, text: 'user'),
-          ]),
+          ],
+          selectedIndex: _selectIndex,
+          onTabChange: (index) {
+            setState(() {
+              _selectIndex = index;
+            });
+          }),
     );
   }
 }
